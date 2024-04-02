5a359aa813cb749239d92056b823eb245bfdfc69
hayeon <hayun4475@gmail.com>
2024-04-02 21:02:10 +0900

    feat: fine tuning 데이터 추가

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
+# app.py
 from flask import Flask
+import threading
+import os
 
 app = Flask(__name__)
 
-@app.route('/') #기존 경로 
-def hello():
-    return 'Hello, My First Flask!'
+def run_scrapy():
+    # Scrapy 프로젝트의 위치로 이동
+    os.chdir('jobkorea')
+    # Scrapy 스크립트 실행
+    os.system('scrapy crawl test')
+
+@app.route('/')
+def hello_world():
+    # 별도의 스레드에서 Scrapy 스크립트 실행
+    threading.Thread(target=run_scrapy).start()
+    return 'Scrapy 스크립트 실행 중!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
