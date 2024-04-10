cd0852439edc5313014688d91363ec394342a6de
hayeon <hayun4475@gmail.com>
2024-04-10 22:31:50 +0900

    fix: rest api  통신오류 해결

--- api/api.py
+++ api/api.py
@@ -1,9 +1,17 @@
-from flask import Blueprint, request, jsonify
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
-api = Blueprint('api', __name__)
+api = Flask(__name__)
+api.config['DEBUG'] = True
+CORS(api)
+
 
 @api.route('/data', methods=['POST'])
-def receive_data():
+def receive_string():
     data = request.json
-    print(data)  # 받은 데이터
-    return jsonify({'message': '데이터 성공!!', 'Data': data})
+    received_string = data['string']
+    print("데이터 도착!", received_string)
+    return jsonify({"response": received_string})
+
+if __name__ == '__main__':
+    api.run(host="0.0.0.0", port="5000",  debug=True)
--- app.py
+++ app.py
@@ -1,20 +1,45 @@
-# app.py
-from flask import Flask
-import threading
-import os
-from api.api import api
+# from flask import Flask, request, jsonify
+# from flask_cors import CORS
 
+# api = Flask(__name__)
+# api.config['DEBUG'] = True
+# CORS(api)
+
+
+# @api.route('/data', methods=['POST'])
+# def receive_string():
+#     data = request.json
+#     received_string = data['string']
+#     print("데이터 도착!", received_string)
+#     return jsonify({"response": received_string})
+
+# if __name__ == '__main__':
+#     api.run(host="0.0.0.0", port="5000",  debug=True)
+    
+
+
+from flask import Flask
+from flask_cors import CORS
+from api.api import api 
+import os
+import threading
 
 app = Flask(__name__)
-app.register_blueprint(api, url_prefix='/api')  # Blueprint 등록
+CORS(app)  
+
+app.register_blueprint(api, url_prefix='/data')  #컴포넌트 분리
+@app.route('/')
+
 def run_scrapy():
     os.chdir('jobkorea')
     os.system('scrapy crawl test')
 
-@app.route('/')
+
+
 def hello_world():
     threading.Thread(target=run_scrapy).start()
     print('Scrapy 스크립트 실행 중!')
+    return 'Scrapy 스크립트가 실행되었습니다!', 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port="5000", debug=True)
